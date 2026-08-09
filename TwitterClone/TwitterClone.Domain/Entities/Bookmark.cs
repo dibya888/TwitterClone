e7f7b36e1119d783178a44c7e70e26b7fe5f2a76
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-08-10 02:05:34 +0600

    Added Bookmark class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,28 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Bookmark
+    {
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _bookmarkedAt;
+
+        public Bookmark(Guid userId, Guid tweetId)
+        {
+            _userId = userId;
+            _tweetId = tweetId;
+            _bookmarkedAt = DateTime.Now;
+        }
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+        public DateTime BookmarkedAt
+        {
+            get { return _bookmarkedAt; }
+        }
+    }
+}
