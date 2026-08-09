5c9de49109275394018ddac6d65aff9e4f524b6f
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-08-10 01:40:07 +0600

    Added Follow class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -0,0 +1,33 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Follow
+    {
+        private Guid _followerId;
+        private Guid _followingId;
+        private DateTime _followedAt;
+
+
+        public Follow(Guid followerId, Guid followingId)
+        {
+            _followerId = followerId;
+            _followingId = followingId;
+            _followedAt = DateTime.Now;
+        }
+
+        public Guid FollowerId
+        {
+            get { return _followerId; }
+        }
+
+        public Guid FollowingId
+        {
+            get { return _followingId; }
+        }
+
+        public DateTime FollowedAt
+        {
+            get { return _followedAt; }
+        }
+    }
+
+}
