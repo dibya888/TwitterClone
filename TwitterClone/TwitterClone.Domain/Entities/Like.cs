775a4dbaec97c9d47d2497871ab215c99e78f84a
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-08-10 01:21:28 +0600

    Added Tweet & Like class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Like.cs
@@ -0,0 +1,30 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Like
+    {
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _likedAt;
+
+        public Like(Guid userId, Guid tweetId)
+        {
+            _userId = userId;
+            _tweetId = tweetId;
+            _likedAt = DateTime.Now;
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+        public Guid TweerId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime LikedAt
+        {
+            get { return DateTime.Now; }
+        }
+    }
+}
---
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -0,0 +1,31 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Tweet
+    {
+        private Guid _id;
+        private Guid _authorId;
+        private string _content;
+
+        public Tweet(Guid authorId)
+        {
+            _id = Guid.NewGuid();
+            _authorId = authorId;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid AuthorId
+        {
+            get { return _authorId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+    }
+}
