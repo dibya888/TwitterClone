b314ea77d924530c2b9f650349e944520a012cd2
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-08-10 01:59:12 +0600

    Added Message class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Message.cs
@@ -0,0 +1,38 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Message
+    {
+        private Guid _senderId;
+        private Guid _receiverId;
+        private string _content;
+        private DateTime _sendAt;
+
+        public Message(Guid senderId, Guid receiverId, string content)
+        {
+            _senderId = senderId;
+            _receiverId = receiverId;
+            _content = content;
+            _sendAt = DateTime.Now;
+        }
+
+        public Guid SenderId
+        {
+            get { return _senderId; }
+        }
+
+        public Guid ReceiverId
+        {
+            get { return _receiverId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+        }
+
+        public DateTime SendAt
+        {
+            get { return _sendAt; }
+        }
+    }
+}
