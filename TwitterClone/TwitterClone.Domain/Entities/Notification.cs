cc3eea73d29e2bbd28afd362119a4b130849e754
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-08-10 01:49:38 +0600

    Added Notification class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Notification.cs
@@ -0,0 +1,31 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Notification
+    {
+        private Guid _userId;
+        private string _message;
+        private DateTime _createdAt;
+
+        public Notification(Guid userId, string message)
+        {
+            _userId = userId;
+            _message = message;
+            _createdAt = DateTime.Now;
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+        }
+    }
+}
