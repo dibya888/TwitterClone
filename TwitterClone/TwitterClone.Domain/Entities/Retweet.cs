73a19b96c6522091d2fe3d00fe6b97b043412cc9
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-08-10 01:32:10 +0600

    Added Retweet class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -0,0 +1,31 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Retweet
+    {
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _retweetedAt;
+
+        public Retweet(Guid userId, Guid tweetId)
+        {
+            _userId = userId;
+            _tweetId = tweetId;
+            _retweetedAt = DateTime.Now;
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime RetweetedAt
+        {
+            get { return _retweetedAt; }
+        }
+    }
+}
